3fae1cd698de0b5a1ea93fcb41c7f6773958427c
saiful haque <saiful haque@DESKTOP-8OI0523>
2018-01-17 17:28:00 -0800

    Version 2

--- Maven_Project/src/test/java/com/usa/learningjava/My_First_java.java
+++ Maven_Project/src/test/java/com/usa/learningjava/My_First_java.java
@@ -3,6 +3,7 @@ package com.usa.learningjava;
 public class My_First_java {
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		System.out.println("hello people");
 	}
 
