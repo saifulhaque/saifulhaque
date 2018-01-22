284d8824ae156686237f147b9675e3989ffb14f4
saiful haque <saiful haque@DESKTOP-8OI0523>
2018-01-21 16:34:48 -0800

    version3

--- Maven_Project/src/test/java/com/usa/learningjava/My_First_java.java
+++ Maven_Project/src/test/java/com/usa/learningjava/My_First_java.java
@@ -1,16 +1,4 @@
-package com.usa.learningjava;
 
-public class My_First_java {
-
-	public static void main(String[] args) {
-		System.out.println("this is main");
-		
-	}
-	public void dosomething() {
-		System.out.println("hi");;
-		
 	
 		
-	}
 
-}
---
+++ Maven_Project/src/test/java/com/usa/talentech/telantech.java
@@ -0,0 +1,37 @@
+package com.usa.talentech;
+
+public class telantech {
+	
+	public void salary() {
+
+	System.out.println("hello world");
+	
+	}
+	public static void income(){
+		System.out.println("hi world");
+		
+	}
+	public static int money() {
+		
+		System.out.println("talentech");
+		return 0;}
+	
+		
+		public static void main(String[] args) {
+			
+			telantech asdf = new telantech();
+			asdf.salary();
+			
+			telantech.income();
+			telantech.money();
+			
+			
+		
+		
+	}
+		
+		
+
+	
+}
+
